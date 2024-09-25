ae0a610bac7b86579731a376b28650465f4ff272
bisht2311 <harshitbisht.2311@gmail.com>
2024-09-25 15:52:14 +0530

    done

--- server/index.js
+++ server/index.js
@@ -18,19 +18,16 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-// const corsOptions = {
-//     origin:true,
-//     credentials:true,
-// }
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true,
+}
 // http://localhost:5173/signup
-app.use(cors())
-const allowCrossDomain = (req, res, next) => {
-    res.header(`Access-Control-Allow-Origin`, `example.com`);
-    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-    next();
-  };
-  app.use(allowCrossDomain);
+
+app.use(cors(
+    corsOptions
+))
+
 
 const PORT = process.env.PORT || 3000
 
