78297fdf91abe550b909509c48381dd05eff5224
bisht2311 <harshitbisht.2311@gmail.com>
2024-09-25 15:21:40 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -18,12 +18,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-const corsOptions = {
-    origin:true,
-    credentials:true,
-}
+// const corsOptions = {
+//     origin:true,
+//     credentials:true,
+// }
 // http://localhost:5173/signup
-app.use(cors(corsOptions))
+app.use(cors())
 
 
 const PORT = process.env.PORT || 3000
