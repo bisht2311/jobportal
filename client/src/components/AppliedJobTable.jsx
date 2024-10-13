dd4882a96aa51d9cdc7a31afddecda341a6c28a8
Harshit Bisht <117760947+bisht2311@users.noreply.github.com>
2024-10-13 11:42:44 +0530

    Update AppliedJobTable.jsx

--- client/src/components/AppliedJobTable.jsx
+++ client/src/components/AppliedJobTable.jsx
@@ -1,37 +1,67 @@
-import React from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { Badge } from './ui/badge'
-import { useSelector } from 'react-redux'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { Badge } from "./ui/badge";
+import { useSelector } from "react-redux";
 
 const AppliedJobTable = () => {
-    const {allAppliedJobs} = useSelector(store=>store.job);
-    return (
-        <div>
-            <Table>
-                <TableCaption>A list of your applied jobs</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Date</TableHead>
-                        <TableHead>Job Role</TableHead>
-                        <TableHead>Company</TableHead>
-                        <TableHead className="text-right">Status</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {
-                        allAppliedJobs.length <= 0 ? <span>You haven't applied any job yet.</span> : allAppliedJobs.map((appliedJob) => (
-                            <TableRow key={appliedJob._id}>
-                                <TableCell>{appliedJob?.createdAt?.split("T")[0]}</TableCell>
-                                <TableCell>{appliedJob.job?.title}</TableCell>
-                                <TableCell>{appliedJob.job?.company?.name}</TableCell>
-                                <TableCell className="text-right"><Badge className={`${appliedJob?.status === "rejected" ? 'bg-red-400' : appliedJob.status === 'pending' ? 'bg-gray-400' : 'bg-green-400'}`}>{appliedJob.status.toUpperCase()}</Badge></TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
-        </div>
-    )
-}
+  const { allAppliedJobs } = useSelector((store) => store.job);
 
-export default AppliedJobTable
+  //   console.log(lenght);
+  //   console.log(allAppliedJobs);
+  //   const allAppliedJobs = [1, 2, 3, 4];
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {allAppliedJobs.length <= 0 ? (
+            <TableRow>
+              <TableCell>You haven't applied any job yet.</TableCell>
+            </TableRow>
+          ) : (
+            allAppliedJobs.map((appliedJob, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  {appliedJob?.createdAt?.split("T")[0] || "17-07-2024"}
+                </TableCell>
+                <TableCell>{appliedJob.job?.title || "Aryanshi"}</TableCell>
+                <TableCell>{appliedJob.job?.company?.name || "Love"}</TableCell>
+                <TableCell className="text-right">
+                  <Badge
+                    className={`${
+                      appliedJob?.status === "rejected"
+                        ? "bg-red-400"
+                        : appliedJob.status === "pending"
+                        ? "bg-gray-400"
+                        : "bg-green-400"
+                    }`}
+                  >
+                    {appliedJob.status.toUpperCase()}
+                  </Badge>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default AppliedJobTable;
