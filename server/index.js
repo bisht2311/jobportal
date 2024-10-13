faca0b6bf13f85196777ef1e15c97a55f17e9766
Harshit Bisht <117760947+bisht2311@users.noreply.github.com>
2024-10-13 12:10:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,37 +4,33 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js"
-import jobRoute from "./routes/job.route.js"
-import applicationRoute from "./routes/application.route.js"
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
-const app = express()
-
+const app = express();
 
 // middleware
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true,
-}
+  origin: "https://jobportal-gamma-three.vercel.app",
+  credentials: true,
+};
 // http://localhost:5173/signup
 
-app.use(cors(
-    corsOptions
-))
+app.use(cors(corsOptions));
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // API's
-app.use("/api/v1/user",userRoute)
-app.use("/api/v1/company",companyRoute)
-app.use("/api/v1/job",jobRoute)
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 /*
@@ -43,10 +39,7 @@ app.use("/api/v1/application", applicationRoute);
 "http://localhost:8000/api/v1/user/profile/update"
 */
 
-app.listen(PORT,()=>{
-    connectDB()
-    console.log(`Server is running at port ${PORT}`)
-})
-
-
-
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running at port ${PORT}`);
+});
