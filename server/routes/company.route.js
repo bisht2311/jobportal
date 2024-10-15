c9addbc92697fff27c65c4cd5b1346393eef1dc2
Harshit Bisht <117760947+bisht2311@users.noreply.github.com>
2024-10-15 10:38:40 +0530

    Update company.route.js

--- server/routes/company.route.js
+++ server/routes/company.route.js
@@ -5,7 +5,7 @@ import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router()
 
-router.route("/register").post(isAuthenticated,registerCompany);
+router.route("/register").post(isAuthenticated,singleUpload,registerCompany);
 router.route("/get").get(isAuthenticated,getCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
 router.route("/update/:id").put(isAuthenticated,singleUpload,updateCompany);
